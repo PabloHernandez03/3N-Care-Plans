d5eb2a47339cae4f6bcb59ca676b52d9182314cf
PonchoPilatos <kevin.aldrete0201@alumnos.udg.mx>
2025-02-25 12:18:59 -0600

    Cambios en CareplansView

--- src/components/care-plans/CarePlanForm.tsx
+++ src/components/care-plans/CarePlanForm.tsx
@@ -13,6 +13,10 @@ interface CarePlanFormData {
 }
 
 const CarePlanForm: React.FC<CarePlanFormProps> = ({ onCancel }) => {
+    const [selectedOption, setSelectedOption] = useState<string>("");
+    const [selectedSex, setSelectedSex] = useState<string>("");
+    // const [searchTerm, setSearchTerm] = useState<string>("");
+
     const [formData, setFormData] = useState<CarePlanFormData>({
         title: '',
         description: '',
@@ -27,6 +31,45 @@ const CarePlanForm: React.FC<CarePlanFormProps> = ({ onCancel }) => {
         setFormData({ ...formData, [name]: value });
     };
 
+    {/**/}
+
+    const handleBirthdate = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+    ) => {
+        const { value } = e.target;
+        const regex = /^[0-9/]*$/;
+        if (regex.test(value)) {
+            for(let i=0; i < value.length; i++){
+                if(i === 2 || i === 5){
+                    if(value.charAt(i) !== '/'){
+                        e.target.value = value.slice(0, -1);
+                    }
+                } else {
+                    if(value.charAt(i) === '/' && i !== 2 && i !== 5){
+                        e.target.value = value.slice(0, -1);
+                    }
+                }
+            }
+                
+        } else {
+            e.target.value = value.slice(0, -1);
+        }
+    };
+
+    const handleAge = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+    ) => {
+        const { value } = e.target;
+        const regex = /^[0-9]*$/;
+        if (regex.test(value)) {
+            if(parseInt(value) > 130){
+                e.target.value = value.slice(0, -1);
+            }
+        } else {
+            e.target.value = value.slice(0, -1);
+        }
+    };
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         console.log('Datos del plan:', formData);
@@ -36,17 +79,91 @@ const CarePlanForm: React.FC<CarePlanFormProps> = ({ onCancel }) => {
 
     return (
         <form onSubmit={handleSubmit} className="space-y-4">
+
             <div>
-                <label className="block text-sm font-medium text-gray-700">Título del Plan</label>
+                <label className="block text-sm font-medium text-gray-700">Paciente</label>
+                <select
+                    name="patientId"
+                    value={selectedOption}
+                    onChange={(e) => setSelectedOption(e.target.value)}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                    required
+                >
+                    <option value="">Seleccione un paciente</option>
+                    {/* <option value="buscar">Buscar paciente...</option> */}
+                    <option value="1">Juan Pérez</option>
+                    <option value="2">María Gómez</option>
+                    <option value="3">Carlos Ruiz</option>
+                </select>
+                
+                {/* {selectedOption === "buscar" && (
+                    <div className="mt-4">
+                        <label className="block text-sm font-medium text-gray-700">Buscar Paciente:</label>
+                        <input
+                            type="text"
+                            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                            placeholder="Escriba el nombre del paciente"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                    </div>
+                )} */}
+            </div>
+
+            { selectedOption !== "" && ( 
+                <div className="flex justify-end gap-4">
+                    <Button type="button" onClick={onCancel} variant="primary">
+                        Ver Historial Clínico
+                    </Button>
+                </div>
+                
+            )}
+
+            {/* DATOS BÁSICOS */}
+            <div>
+                <label className="block text-sm font-medium text-gray-700">Edad</label>
                 <input
-                    type="text"
-                    name="title"
-                    value={formData.title}
-                    onChange={handleChange}
+                    placeholder='Edad del paciente' 
+                    type="text" //patientAge
+                    name="age"
+                    maxLength={3}
+                    // value={formData.title}
+                    onChange={handleAge}
                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                     required
                 />
             </div>
+
+            <label className="block text-sm font-medium text-gray-700">Sexo</label>
+            <select
+                name="patientSex"
+                value={selectedSex}
+                onChange={(e) => setSelectedSex(e.target.value)}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                required
+            >   
+                <option value="">Seleccione el sexo del paciente</option>
+                <option value="M">Masculino</option>
+                <option value="F">Femenino</option>
+                <option value="N">Pinche helicoptero apache alv</option>
+            </select>
+
+            { /* --/--/---- */ }
+            <div>
+                <label className="block text-sm font-medium text-gray-700">Fecha de nacimiento</label>
+                <input
+                    placeholder='--/--/----'
+                    type="text"
+                    name="patientBirthdate"
+                    // value={formData.birthdate}
+                    onChange={handleBirthdate}
+                    maxLength={10}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                    pattern="\d{2}/\d{2}/\d{4}"
+                    required
+                />
+            </div>
+            
 
             <div>
                 <label className="block text-sm font-medium text-gray-700">Descripción</label>
@@ -58,37 +175,6 @@ const CarePlanForm: React.FC<CarePlanFormProps> = ({ onCancel }) => {
                     rows={4}
                     required
                 />
-            </div>
-
-            <div>
-                <label className="block text-sm font-medium text-gray-700">Paciente</label>
-                <select
-                    name="patientId"
-                    value={formData.patientId}
-                    onChange={handleChange}
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                    required
-                >
-                    <option value="">Seleccione un paciente</option>
-                    <option value="1">Juan Pérez</option>
-                    <option value="2">María Gómez</option>
-                    <option value="3">Carlos Ruiz</option>
-                </select>
-            </div>
-
-            <div>
-                <label className="block text-sm font-medium text-gray-700">Frecuencia</label>
-                <select
-                    name="frequency"
-                    value={formData.frequency}
-                    onChange={handleChange}
-                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                    required
-                >
-                    <option value="diario">Diario</option>
-                    <option value="semanal">Semanal</option>
-                    <option value="mensual">Mensual</option>
-                </select>
             </div>
 
             <div className="flex justify-end gap-4">
